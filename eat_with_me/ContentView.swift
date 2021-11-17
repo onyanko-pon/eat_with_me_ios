5e7047d20418c903fd2f1fa0210ed5909f066be6
onyanko-pon <engineer.tsukasa@gmail.com>
2021-11-17 16:52:14 +0900

    Initial Commit

---
+++ eat_with_me/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  eat_with_me
+//
+//  Created by 丸山司 on 2021/11/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ eat_with_me/eat_with_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  eat_with_meApp.swift
+//  eat_with_me
+//
+//  Created by 丸山司 on 2021/11/17.
+//
+
+import SwiftUI
+
+@main
+struct eat_with_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
